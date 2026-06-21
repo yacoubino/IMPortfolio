6dd441c0b9f908418c47706c90a3e0285f0836fb
yacoubino <yacoubino23@gmail.com>
2026-06-21 12:52:13 +0100

    website

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thank you! Your message has been sent.");
+    this.reset();
+  });
+  
